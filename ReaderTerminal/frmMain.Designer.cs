7e22bfa174197a17a03d986d35178e8cc41926ea
EUYUIL <euyuil@gmail.com>
2012-05-11 09:20:27 +0800

    用户界面文档基本框架已形成。

--- ReaderTerminal/frmMain.Designer.cs
+++ ReaderTerminal/frmMain.Designer.cs
@@ -39,7 +39,6 @@
             this.cmbSearchType = new System.Windows.Forms.ComboBox();
             this.btnSearch = new System.Windows.Forms.Button();
             this.txtSearchContent = new System.Windows.Forms.TextBox();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.mnuMain = new System.Windows.Forms.MenuStrip();
             this.终端TToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuTerminalExit = new System.Windows.Forms.ToolStripMenuItem();
@@ -73,7 +72,6 @@
             // tabMain
             // 
             this.tabMain.Controls.Add(this.tpgBookSearch);
-            this.tabMain.Controls.Add(this.tabPage2);
             this.tabMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabMain.Location = new System.Drawing.Point(7, 0);
             this.tabMain.Name = "tabMain";
@@ -182,16 +180,6 @@
             this.txtSearchContent.Name = "txtSearchContent";
             this.txtSearchContent.Size = new System.Drawing.Size(250, 21);
             this.txtSearchContent.TabIndex = 1;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(444, 225);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // mnuMain
             // 
@@ -309,7 +297,6 @@
         private System.Windows.Forms.ComboBox cmbSearchType;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.TextBox txtSearchContent;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.MenuStrip mnuMain;
         private System.Windows.Forms.ToolStripMenuItem 读者RToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem mnuReaderLogin;
